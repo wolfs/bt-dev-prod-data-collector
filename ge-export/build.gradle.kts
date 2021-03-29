13d213c0377d40a438187a1cbcf8c1b6391f9e99
Stefan Wolf <wolf@gradle.com>
2021-03-29 19:03:28 +0200

    Add actuator to have some health checks

--- ge-export/build.gradle.kts
+++ ge-export/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-jooq")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
